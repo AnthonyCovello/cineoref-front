7dd951d11120d59dad2b73b9041b24878e366543
AnthonyCovello <alphatek@hotmail.fr>
2022-05-05 09:34:13 +0200

    structure header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,6 @@
 // ? Import modules
 import React from 'react';
+import { FcFilmReel } from 'react-icons/fc';
 // import { Routes, Route, useLocation } from 'react-router-dom';
 // import { useDispatch, useSelector } from 'react-redux';
 
@@ -12,7 +13,24 @@ import './styles.scss';
 function App() {
   return (
     <div className="app">
-      <h1>Composant : App</h1>
+      <header className="header">
+        <a href="/" className="header-brand">
+          <FcFilmReel className="header-brand-logo" />
+          <span className="header-brand-title">CinO'Ref</span>
+        </a>
+        <nav className="header-navbar">
+          <a href="#" className="header-navbar-button">Films</a>
+          <a href="#" className="header-navbar-button">Séries</a>
+          <a href="#" className="header-navbar-button">Animés</a>
+          <a href="#" className="header-navbar-button">Dessins animés</a>
+          <a href="#" className="header-navbar-button">Artistes</a>
+          <a href="#" className="header-navbar-button">Personnages</a>
+        </nav>
+        <div className="header-connexion">
+          <span className="signIn_button">Connexion</span>
+          <span className="signUp_button">Inscription</span>
+        </div>
+      </header>
     </div>
   );
 }
