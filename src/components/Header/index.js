979020d055b67a4228bfb73208da08d3c93edabe
AnthonyCovello <alphatek@hotmail.fr>
2022-05-05 16:03:28 +0200

    component header OK + style

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,17 +9,17 @@ import './styles.scss';
 function Header() {
   return (
     <header className="header">
-      <a href="/" className="header-brand">
+      <a href="/" title="Page d'accueil" className="header-brand">
         <FcFilmReel className="header-brand-logo" />
-        <span className="header-brand-title">CinO'Ref</span>
+        <span className="header-brand-title">Cin<span>O</span>'Ref</span>
       </a>
       <nav className="header-navbar">
-        <a href="#" className="header-navbar-button">Films</a>
-        <a href="#" className="header-navbar-button">Séries</a>
-        <a href="#" className="header-navbar-button">Animés</a>
-        <a href="#" className="header-navbar-button">Dessins animés</a>
-        <a href="#" className="header-navbar-button">Artistes</a>
-        <a href="#" className="header-navbar-button">Personnages</a>
+        <a href="/films" className="header-navbar-button">Films</a>
+        <a href="/series" className="header-navbar-button">Séries</a>
+        <a href="/animes" className="header-navbar-button">Animés</a>
+        <a href="/cartoons" className="header-navbar-button">Dessins animés</a>
+        <a href="/artists" className="header-navbar-button">Artistes</a>
+        <a href="/characters" className="header-navbar-button">Personnages</a>
       </nav>
       <div className="header-connexion">
         <span className="signIn_button">Connexion</span>
