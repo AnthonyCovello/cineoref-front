5e95d58a286ae4f79391ced98660c7c45b2ad939
Vincent de Almeida <dealmeida.vincent@gmail.com>
2022-05-06 19:26:15 +0200

    TopNew Updated with CSS

--- src/components/Homepage/TopNew/index.js
+++ src/components/Homepage/TopNew/index.js
@@ -5,7 +5,13 @@ import { setTablist } from '../../../feature/topNewSlice';
 // ? Import style
 import './styles.scss';
 
-const topRated = ['blabla1', 'blabla2', 'blabla3', 'blablacar', 'blabla5'];
+const topRated = [
+  'J’ai rêvé qu’il y avait des scorpions qui voulaient me piquer. En plus, y en avait un il était mi-ours, mi-scorpion et re mi-ours derrière !',
+  'Moi, je m’en fous, si on me force à y retourner, je retiens ma respiration jusqu’à ce qu’on arrête de me forcer à y retourner.',
+  'Oh vous, toujours vous, mais allez chier dans une fiole, on verra après.',
+  'Ces conneries de gauche et de droite ! Ça veut rien dire ces machins ! Selon comme on est tourné ça change tout !',
+  'Faut pas respirer la compote, ça fait tousser.',
+];
 
 const newests = ['blibli1', 'blibli2', 'blibli3', 'bliblicar', 'blibli5'];
 
@@ -13,41 +19,44 @@ const newests = ['blibli1', 'blibli2', 'blibli3', 'bliblicar', 'blibli5'];
 function HomeList() {
   const tabList = useSelector(({ topNew }) => topNew.tabList);
   const dispatch = useDispatch();
-  const handleTabs = (event) => {
-    dispatch(setTablist(event.target.value));
-  };
 
   return (
     <div className="citation">
       <nav className="citation-nav">
-        <button
-          type="button"
-          className="citation-nav-btn"
-          onClick={handleTabs}
-          value="topRated"
+        <div
+          onClick={() => dispatch(setTablist('topRated'))}
+          className={
+            tabList !== 'topRated'
+              ? 'citation-nav-topleft inactiveTab'
+              : 'citation-nav-topleft'
+          }
         >
-          Les mieux notées
-        </button>
-        <button
-          type="button"
-          className="citation-nav-btn"
-          onClick={handleTabs}
-          value="newests"
+          <span>Les mieux notées</span>
+        </div>
+        <div
+          onClick={() => dispatch(setTablist('newests'))}
+          className={
+            tabList !== 'newests'
+              ? 'citation-nav-topright inactiveTab'
+              : 'citation-nav-topright'
+          }
         >
-          Les plus récentes
-        </button>
+          <span>Les plus récentes</span>
+        </div>
       </nav>
       <div>
-        <ul>
+        <ul className="refList">
           {tabList === 'topRated'
             ? topRated.map((ref) => (
-              <li key={ref} className="refList">
+              <li key={ref} className="refList-item">
                 <p>{ref}</p>
+                <p>Personnage</p>
               </li>
             ))
             : newests.map((ref) => (
-              <li key={ref} className="refList">
+              <li key={ref} className="refList-item">
                 <p>{ref}</p>
+                <p>Personnage</p>
               </li>
             ))}
         </ul>
