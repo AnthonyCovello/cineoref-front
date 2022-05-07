20f8f62eebe318f6c685680b986a8f1337007361
AnthonyCovello <alphatek@hotmail.fr>
2022-05-07 15:30:30 +0200

    add Links on footer + name change in CinéO'Ref

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,26 +1,28 @@
 // ? Import modules
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FcFilmReel } from 'react-icons/fc';
+
 // ? Import style
 import './styles.scss';
-// ? Composant
 
+// ? Composant
 function Footer() {
   return (
     <footer className="footer">
-      <a href="/" className="footer-brand">
+      <Link to="/" title="Page d'accueil" className="footer-brand">
         <FcFilmReel className="footer-brand-logo" />
         <span className="footer-brand-title-copyright">
-          <span className="footer-brand-title">Cin<span>O</span>'Ref</span>
+          <span className="footer-brand-title">Ciné<span>O</span>'Ref</span>
           <span className="copyright">© Copyright 2022</span>
         </span>
-      </a>
+      </Link>
       <nav className="footer-navbar">
-        <a href="#">Proposer sa citation</a>
-        <a href="#">L'équipe</a>
-        <a href="#">Contact</a>
-        <a href="#">Mentions légales</a>
-        <a href="#">Conditions générales d'utilisation</a>
+        <Link to="/proposal">Proposer sa citation</Link>
+        <Link to="/team">L'équipe</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/legal-policies">Mentions légales</Link>
+        <Link to="/cgu">Conditions générales d'utilisation</Link>
       </nav>
     </footer>
   );
