427df8129fcc1d5148fe70ab9938923b3eeab13e
AnthonyCovello <alphatek@hotmail.fr>
2022-05-07 16:41:29 +0200

    add fictive data

--- src/components/Homepage/TopNew/index.js
+++ src/components/Homepage/TopNew/index.js
@@ -5,6 +5,9 @@ import { setTablist } from '../../../features/topNewSlice';
 // ? Import style
 import './styles.scss';
 
+// ? Data fictives
+import data from '../../../assets/data';
+
 const topRated = [
   'J’ai rêvé qu’il y avait des scorpions qui voulaient me piquer. En plus, y en avait un il était mi-ours, mi-scorpion et re mi-ours derrière !',
   'Moi, je m’en fous, si on me force à y retourner, je retiens ma respiration jusqu’à ce qu’on arrête de me forcer à y retourner.',
@@ -12,7 +15,6 @@ const topRated = [
   'Ces conneries de gauche et de droite ! Ça veut rien dire ces machins ! Selon comme on est tourné ça change tout !',
   'Faut pas respirer la compote, ça fait tousser.',
 ];
-
 const newests = ['blibli1', 'blibli2', 'blibli3', 'bliblicar', 'blibli5'];
 
 // ? Composant
