65306bf292d3d3c3fdb25957adb8b6535333cbfb
AnthonyCovello <alphatek@hotmail.fr>
2022-05-09 20:00:03 +0200

    randomRef button work

--- src/components/Homepage/RandomRef/index.js
+++ src/components/Homepage/RandomRef/index.js
@@ -1,28 +1,25 @@
 // ? Import modules
-import React from 'react';
+import React, { useState } from 'react';
 import randomDice from '../../../assets/randomDice.png';
 import findRandomRef from '../../../utlis';
 
 // ? Import style
 import './styles.scss';
 
-const { ref, character } = findRandomRef();
-console.log('randomResult - ', ref, character);
-
-// const getNewRandomRef = () => {
-//   console.log(findRandomRef());
-//   findRandomRef();
-// };
-
 // ? Composant
 function RandomRef() {
+  const [randomRef, setRandomRef] = useState(findRandomRef());
+  const getNewRandomRef = () => {
+    setRandomRef(findRandomRef());
+  };
+
   return (
     <div className="randomRef">
       <span type="button" className="randomRef-dice">
-        <img src={randomDice} alt="dé à 20 faces" onClick={{/* getNewRandomRef */}} />
+        <img src={randomDice} alt="dé à 20 faces" onClick={getNewRandomRef} />
       </span>
-      <p className="randomRef-text">{ref}</p>
-      <span className="randomRef-author">{character}</span>
+      <p className="randomRef-text">{randomRef.ref}</p>
+      <span className="randomRef-author">{randomRef.character}</span>
       <span className="randomRef-cc">cc</span>
     </div>
   );
