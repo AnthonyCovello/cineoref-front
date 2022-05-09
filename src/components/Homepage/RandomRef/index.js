c7849996fef05565a6a2fd53374a7261b966c39e
AnthonyCovello <alphatek@hotmail.fr>
2022-05-09 21:22:12 +0200

    rotation of the dice OK

--- src/components/Homepage/RandomRef/index.js
+++ src/components/Homepage/RandomRef/index.js
@@ -16,11 +16,11 @@ function RandomRef() {
   return (
     <div className="randomRef">
       <span type="button" className="randomRef-dice">
-        <img src={randomDice} alt="dé à 20 faces" onClick={getNewRandomRef} />
+        <img src={randomDice} title="Afficher une citation aléatoire" alt="dé à 20 faces" onClick={getNewRandomRef} />
       </span>
       <p className="randomRef-text">{randomRef.ref}</p>
       <span className="randomRef-author">{randomRef.character}</span>
-      <span className="randomRef-cc">cc</span>
+      <span className="randomRef-cc" title="Copier le texte">cc</span>
     </div>
   );
 }
