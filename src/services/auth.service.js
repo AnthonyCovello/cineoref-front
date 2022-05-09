9dd47bee168906f7951ba92aec902ed4c036d7c6
Florian-LeCossec <florian.le-cossec@orange.fr>
2022-05-09 10:08:03 +0200

    adding auth-header in services

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -2,9 +2,32 @@ import axios from 'axios';
 
 const API_URL = 'http://:3000';
 
-const register = (pseudo, email, password, birthday) => axios.post(`${API_URL}/login`, {
+const register = (pseudo, email, password, birthday) => axios.post(`${API_URL}/signup`, {
   pseudo,
   email,
   password,
   birthday,
 });
+
+const login = (pseudo, password) => axios.post(`${API_URL}/login`, {
+  pseudo,
+  password,
+})
+  .then((response) => {
+    if (response.data.accessToken) {
+      localStorage.setItem('user', JSON.stringify(response.data));
+    }
+    return response.data;
+  });
+
+const logout = () => {
+  localStorage.removeItem('user');
+};
+
+const authService = {
+  register,
+  login,
+  logout,
+};
+
+export default authService;
