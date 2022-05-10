3e7ec99753af8d950999de475b43e76ff9052aa4
Florian-LeCossec <florian.le-cossec@orange.fr>
2022-05-10 15:54:35 +0200

    mchange path of dropdownSlice in store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 // ? Import modules
 import { configureStore } from '@reduxjs/toolkit';
-import dropdownReducer from './features/dropDownLoginSlice';
+import dropdownReducer from './features/dropDownSlice';
 import topNewReducer from './features/topNewSlice';
 import authReducer from './features/authSlice';
 import messageReducer from './features/messageSlice';
