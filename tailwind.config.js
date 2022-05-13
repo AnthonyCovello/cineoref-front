103f87e0b7892faafe05242d4ba27c09144ca433
Florian-LeCossec <florian.le-cossec@orange.fr>
2022-05-13 15:25:20 +0200

    modif on main on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ module.exports = {
   content: ['./src/components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     screens: {
-      'tablet': '640px',
-      'laptop': '1024px',
-      'desktop': '1280px',
+      tablet: '640px',
+      laptop: '1024px',
+      desktop: '1280px',
     },
     extend: {},
   },
