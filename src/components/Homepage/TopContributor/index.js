40323933677145b6d59b1c70af4574d5d9d479a8
AnthonyCovello <alphatek@hotmail.fr>
2022-05-15 22:09:51 +0200

    change design and styles of top-contributors with Tailwind

--- src/components/Homepage/TopContributor/index.js
+++ src/components/Homepage/TopContributor/index.js
@@ -5,16 +5,16 @@ import { useSelector } from 'react-redux';
 // ? Import style
 import './styles.scss';
 
+// ? Composant
 function TopContributor() {
-  // eslint-disable-next-line max-len
   const contributorsData = useSelector(({ contributors }) => contributors.topContributors);
 
   return (
-    <div className="contributors">
-      <h2 className="contributors-title">Top contributeurs</h2>
+    <div className="contributors w-1/5 p-8 cursor-context-menu text-[1.25rem] font-bold rounded-3xl">
+      <h2 className="contributors-title mb-2.5 text-3xl text-center">Top contributeurs</h2>
       <ul>
         {contributorsData.map((contributor, index) => (
-          <li className="contributors-items" key={contributor.username}>
+          <li className="p-1.5" key={contributor.username}>
             <a href="#" className="contributors-links"> {index + 1} - {contributor.username}</a>
           </li>
         ))}
