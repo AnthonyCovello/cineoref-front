2e82866d5414a90abd59619a02e7d482201fbf13
AnthonyCovello <alphatek@hotmail.fr>
2022-05-15 15:16:15 +0200

    change background image + style description component with Tailwind

--- src/components/Homepage/Description/index.js
+++ src/components/Homepage/Description/index.js
@@ -7,8 +7,8 @@ import './styles.scss';
 // ? Composant
 function Description() {
   return (
-    <div className="description">
-      <p className="description-text">
+    <div>
+      <p className="description-text w-2/5 inset-x-1/3 p-12 m-auto mt-20 font-medium text-center text-[1.5rem] text-porange rounded-3xl">
         Bienvenue sur CinéO'Ref !<br />
         Notre but est de répertorier les citations cinématographiques les plus drôles
         et permettre à tout le monde de trouver celle qu’il cherche ou de simplement
