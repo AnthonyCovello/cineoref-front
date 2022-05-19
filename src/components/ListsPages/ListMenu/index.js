8d5e1cbf1f13a3d0611513322a0059c919813e15
Vincent de Almeida <dealmeida.vincent@gmail.com>
2022-05-19 15:05:14 +0200

    listcss OK no-responsive

--- src/components/ListsPages/ListMenu/index.js
+++ src/components/ListsPages/ListMenu/index.js
@@ -6,9 +6,9 @@ import PropTypes from 'prop-types';
 // ? Import style
 function ListMenu({ listFilters }) {
   return (
-    <nav className="listmenu">
+    <nav className="listmenu text-[150%] pb-[1rem] font-bold flex justify-between items-center">
       {listFilters.map((listFilter) => (
-        <AnchorLink key={listFilter} href={`#${listFilter}`}>{listFilter}</AnchorLink>
+        <AnchorLink key={listFilter} href={`#${listFilter}`} className="anchorletter hover:text-porange hover:scale-150 transition duration 150 ease-out hover:ease-in">{listFilter}</AnchorLink>
       ))}
     </nav>
   );
