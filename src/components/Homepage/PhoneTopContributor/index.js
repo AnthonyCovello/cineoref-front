82ded571a0b8a7a70b6eb780132fea335bc92420
Florian-LeCossec <florian.le-cossec@orange.fr>
2022-05-20 16:35:26 +0200

    add topContributor for phone user in a modal

---
+++ src/components/Homepage/PhoneTopContributor/index.js
@@ -0,0 +1,27 @@
+// ? Import modules
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+// ? Import style
+import './styles.scss';
+
+// ? Composant
+function PhoneTopContributor() {
+  const contributorsData = useSelector(({ contributors }) => contributors.topContributors);
+
+  return (
+    <div className="contributors-phone top-26 left-16 w-3/5 p-2 font-bold rounded-md absolute max-h-min truncate">
+      <h2 className="contributors-title mb-2.5 text-[1.1rem] text-center">Top contributeur</h2>
+      <ul>
+        {contributorsData.map((contributor, index) => (
+          <li className="p-1.5" key={contributor.username}>
+            <Link to={`/user/${contributor.id}/profile`} className="contributors-links"> {index + 1} - {contributor.username}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default React.memo(PhoneTopContributor);
--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,13 +1,14 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 // ? Import modules
 import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { setLoginDropdown } from '../../features/dropDownSlice';
 import { changeTabTitle } from '../../utlis';
 import { setNewRefData } from '../../features/refSlice';
 import { setTopContributorsData } from '../../features/topContributorsSlice';
+import { FaTrophy } from 'react-icons/fa';
 
 // ? Import composants
 import RandomRef from './RandomRef';
@@ -17,6 +18,7 @@ import TopNew from './TopNew';
 
 // ? Import style
 import './styles.scss';
+import PhoneTopContributor from './PhoneTopContributor';
 
 // ? Composant
 function Homepage() {
@@ -26,6 +28,7 @@ function Homepage() {
   const dispatch = useDispatch();
   const isLogged = useSelector(({ auth }) => auth.isLoggedIn);
   const isOpen = useSelector(({ dropdown }) => dropdown.dropdownLogin);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     axios
@@ -61,6 +64,10 @@ function Homepage() {
         if (isOpen === true) toggleDropdown();
       }}
     >
+      <FaTrophy
+        className="icon-trophy-style absolute w-10 h-10 p-1 z-10 top-36 right-2 rounded-full cursor-pointer desk:hidden"
+        onClick={() => setShowModal(!showModal)}
+      />
       <SearchBarRef />
       <div className="flex justify-center gap-20">
         <RandomRef />
@@ -70,6 +77,8 @@ function Homepage() {
         Ajouter une ref'
       </span>
       <TopNew />
+
+      {showModal && <PhoneTopContributor />}
     </div>
   );
 }
