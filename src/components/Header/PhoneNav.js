5ea05ccba69994c58121d78247e378dbef64e97e
AnthonyCovello <alphatek@hotmail.fr>
2022-05-20 03:54:44 +0200

    fix phoneNav center

--- src/components/Header/PhoneNav.js
+++ src/components/Header/PhoneNav.js
@@ -11,17 +11,17 @@ function PhoneNav() {
   const toggleMenu = () => setOpenMenu(!openMenu);
 
   return (
-    <div className="phoneNav font-bold">
+    <div className="phoneNav absolute left-[45%] font-bold phone:left-[42%]">
       <button
         type="button"
-        className="menu-button font-bold rounded"
+        className="menu-button py-2 px-3 font-bold rounded phone:py-1 phone:px-2"
         onClick={toggleMenu}
       >
         Menu
       </button>
       {openMenu
         && (
-          <nav className="navMenu flex flex-col w-full text-center" onClick={toggleMenu}>
+          <nav className="navMenu absolute top-14 left-[-29.5rem] flex flex-col text-center phone:top-10" onClick={toggleMenu}>
             <Link to="/listcategory/movie">Films</Link>
             <Link to="/listcategory/serie">Séries</Link>
             <Link to="/listcategory/anime">Animés</Link>
