7aded62439c60e5d70080e89fc017fa34b4983ec
AnthonyCovello <alphatek@hotmail.fr>
2022-05-24 07:58:56 +0200

    add protected route for admin dashboard

---
+++ src/components/AdminDashboard/index.js
@@ -0,0 +1,16 @@
+// ? Import modules
+import React from 'react';
+
+// ? Import composants
+
+// ? Import styles
+import './styles.scss';
+
+// ? Composant
+function AdminDashboard() {
+  return (
+    <h1>Interface admin</h1>
+  );
+}
+
+export default React.memo(AdminDashboard);
--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,11 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/jsx-no-comment-textnodes */
 // ? Import modules
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 // ? Import composants
+import AdminDashboard from '../AdminDashboard';
 import Header from '../Header';
 import Footer from '../Footer';
 import Homepage from '../Homepage';
@@ -23,6 +24,7 @@ import './styles.scss';
 // ? Composant
 function App() {
   const isLogged = useSelector(({ auth }) => auth.isLoggedIn);
+  const user = useSelector(({ auth }) => auth.user);
 
   return (
     <div className="app relative m-0 pt-28 tracking-[1.5px] phone:pt-20 tablet:pt-24">
@@ -31,18 +33,20 @@ function App() {
 
       <Routes>
 
-        <Route path="/" element={<Homepage />} />
-        <Route path="/searchResult" element={<SearchResult />} />
-        <Route path="/registration" element={<SignUp />} />
-        <Route path="/proposal" element={isLogged ? <ProposalForm /> : <Navigate to="/" replace />} />
-        <Route path="/user/:id/my-profile" element={<Profile />} />
-        <Route path="/user/:id/profile" element={<Profile />} />
-        <Route path="/:listTheme" element={<ListsPages />} />
-        <Route path="/:listTheme/:param" element={<ListsPages />} />
-        <Route path="/:listTheme/:category/:id/refs" element={<ListsRef />} />
-        <Route path="/ref/:ref_id" element={<RefPage />} />
+        <Route path="/" element={<Homepage />} /> // * Page d'accueil
+        <Route path="/searchResult" element={<SearchResult />} /> //* Page de résultats de recherche
+        <Route path="/registration" element={<SignUp />} /> //* Page d'inscription
+        <Route path="/proposal" element={isLogged ? <ProposalForm /> : <Navigate to="/" replace />} /> // * Formulaire de proposition de citation - connexion requise
+        <Route path="/user/:id/my-profile" element={<Profile />} /> // * Profil personnel
+        <Route path="/user/:id/profile" element={<Profile />} /> //* Profil d'un autre utilisateur
+        <Route path="/:listTheme" element={<ListsPages />} /> // * Liste des artistes / personnages
+        <Route path="/:listTheme/:param" element={<ListsPages />} /> // * Lsite des médias films / séries / animés / dessins animés
+        <Route path="/:listTheme/:category/:id/refs" element={<ListsRef />} /> // * Liste des citations associées à un film / série / animé / dessins animés / artiste / personnage
+        <Route path="/ref/:ref_id" element={<RefPage />} /> // * Page d'une citation
 
-        <Route path="*" element={<h1>Error</h1>} />
+        <Route path="*" element={<h1>Error</h1>} /> // * 404
+
+        <Route path="/admin" element={(isLogged && (user.role === 'Fondateur' || user.role === 'Admin')) ? <AdminDashboard /> : <Navigate to="/" replace />} /> //* Interface admin
 
       </Routes>
 
